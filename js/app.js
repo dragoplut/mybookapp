a7a6cec61247bfc787dcc7386a30b8adb3abf794
dragoplut <oleksandr_p@i.ua>
2016-02-01 20:07:52 +0200

    handlebars & stars

--- js/app.js
+++ js/app.js
@@ -3,34 +3,40 @@
  */
 'use strict';
 
-for (var i = 0; i < books.length; i++){
-    var tmpRating = books[i].rating;
-    //var tmpPrice = books[i].price;
+$(function(){
+    var source = '{{#each .}}' +
+        '<div id="{{ID}}" class="book-list">' +
+        '<img src="{{cover}}">' +
+        '<a class="book-list-title" href="#">{{title}}</a>' +
+        '<a class="book-list-author" href="#">by {{author}}</a>' +
+        '</div>' +
+    '{{/each}}';
+    var template = Handlebars.compile(source);
+    var html = template(books);
+    $('.main-book-list').append(html);
+});
+
+
+/**
+ * Render rating "stars" html string
+ * Param required: integer, from '0' to '5', step: '0.5'
+ * @param tmpRating
+ * @returns {string}
+ */
+function renderRating(tmpRating){
 
     var renderRating = '<span class="star-orange">';
-
-    for (var j = 0; j < 5; j++){
-        if (tmpRating >= 1){
+    for (var j = 0; j < 5; j++) {
+        if (tmpRating >= 1) {
             renderRating += '<i class="fa fa-star"></i>';
-        } else if (tmpRating == 0.5){
+        } else if (tmpRating == 0.5) {
             renderRating += '<i class="fa fa-star-half-o"></i>';
         } else {
             renderRating += '<i class="fa fa-star-o"></i>';
         }
         tmpRating = tmpRating - 1;
     }
-
     renderRating += '</span>';
 
-    var elemTemplate = '<div id="{{tmpId}}" class="book-list">'+
-            '<img src="{{tmpCover}}">'+
-            '<a class="book-list-title" href="#">{{tmpTitle}}</a>'+
-            '<a class="book-list-author" href="#">by {{tmpAuthor}}</a>'+ renderRating +
-        '</div>';
-
-    var template = Handlebars.compile(elemTemplate);
-
-    var html = template({tmpAuthor: books[i].author, tmpCover: books[i].cover, tmpId: books[i].ID, tmpTitle: books[i].title});
-
-    $('.main-book-list').append(html);
+    return renderRating;
 }
