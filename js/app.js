eca64dc9cd8746239e1f584396c030dcf3a9748c
dragoplut <oleksandr_p@i.ua>
2016-02-04 14:42:16 +0200

    IIFE works

--- js/app.js
+++ js/app.js
@@ -3,45 +3,6 @@
  */
 'use strict';
 
-/**
- * Handlebars template
- * Forbidden to change
- * @type {*|jQuery}
- */
-var source = $('#books-template').html();
-
-$(function(){
-    renderBooks(books);
-});
-
-$('.btn-all-bo').on('click', function () {
-    renderBooks(books);
-});
-
-$('.btn-most-re').on('click', function (){
-    renderBooks(sortBy('date'));
-});
-
-$('.btn-most-po').on('click', function (){
-    renderBooks(sortBy('rating'));
-});
-
-$('.btn-free-bo').on('click', function (){
-    var free = 0;
-    renderBooks(searchNew(free));
-});
-
-$('.input-search-books').keypress(function(event){
-    if (event.keyCode == 13){
-        renderBooks(searchNew(this.value));
-    }
-});
-
-$('.input-search-fa-icon').on('click', function(){
-    var item = $('.input-search-books').val();
-    renderBooks(searchNew(item));
-});
-
 Handlebars.registerHelper('loop', function(count, options){
     var out = '';
     while (count--){
@@ -50,45 +11,64 @@ Handlebars.registerHelper('loop', function(count, options){
     return out;
 });
 
-function renderBooks(books){
-    console.info('renderBooks');
-    var template = Handlebars.compile(source);
-    var html = template(books);
-    $('.main-book-list').html(html);
-}
+var BooksModule = (function(get){
 
-function searchNew (item){
-    var result = [];
-    if (item != 0){
-        item = item.toLowerCase();
+    /**
+     * Handlebars template
+     * Forbidden to change
+     * @type {*|jQuery}
+     */
+    var source = $('#books-template').html();
+    var books = get;
+
+    (function init(){
+        renderBooks(books);
+    }());
+
+    function renderBooks(books){
+        console.info('renderBooks');
+        var template = Handlebars.compile(source);
+        var html = template(books);
+        $('.main-book-list').html(html);
     }
-    for (var i = 0; i < books.length; i++){
-        var searchTitle = books[i].title.toLowerCase();
-        var searchAuthor = books[i].author.toLowerCase();
-        if (searchTitle.indexOf(item) > -1 || searchAuthor.indexOf(item) > -1 || books[i].price === item){
-            result.push(books[i]);
+
+    function searchNew (item){
+        var result = [];
+        if (item != 0){
+            item = item.toLowerCase();
         }
+        for (var i = 0; i < books.length; i++){
+            var searchTitle = books[i].title.toLowerCase();
+            var searchAuthor = books[i].author.toLowerCase();
+            if (searchTitle.indexOf(item) > -1 || searchAuthor.indexOf(item) > -1 || books[i].price === item){
+                result.push(books[i]);
+            }
+        }
+        renderBooks(result);
     }
-    return result;
-}
 
-function sortBy (option){
-    var sorted = $.extend([], books);
-    console.info(sorted);
-    if (option == 'rating'){
-        sorted.sort(function (a, b) {
-            var a1 = a.rating;
-            var b1 = b.rating;
-            if (a1 == b1) return 0;
-            return a1 < b1 ? 1 : -1;
-        });
-    } else if (option == 'date'){
-        sorted.sort(function (a, b) {
-            var a1 = a.date;
-            var b1 = b.date;
-            if (a1 == b1) return 0;
-            return a1 < b1 ? 1 : -1;
-        });
+    function sortBy (option){
+        var sorted = $.extend([], books);
+        console.info(sorted);
+        if (option == 'rating'){
+            sorted.sort(function (a, b) {
+                var a1 = a.rating;
+                var b1 = b.rating;
+                if (a1 == b1) return 0;
+                return a1 < b1 ? 1 : -1;
+            });
+        } else if (option == 'date'){
+            sorted.sort(function (a, b) {
+                var a1 = a.date;
+                var b1 = b.date;
+                if (a1 == b1) return 0;
+                return a1 < b1 ? 1 : -1;
+            });
+        }
+        renderBooks(sorted);
     }
-    return sorted;
-}
+    return {
+        searchNew: searchNew,
+        sortBy: sortBy
+    };
+}(DataModule.get));
--- js/data.js
+++ js/data.js
@@ -1,95 +1,98 @@
 /**
  * Created by oleksandr on 01.02.16.
  */
-var books = [
-    {
-        ID: 1,
-        title: 'Jewels of Nizam',
-        author: 'Geeta Devi',
-        rating: 5,
-        price: 100,
-        date: 2015,
-        cover: 'img/book-cover.png'
-    },
-    {
-        ID: 2,
-        title: 'Cakes & Bakes',
-        author: 'Sanjeev Kapoor',
-        rating: 5,
-        price: 80,
-        date: 2012,
-        cover: 'img/book-cover%20(1).png'
-    },
-    {
-        ID: 3,
-        title: 'Jamie\'s Kitchen',
-        author: 'Jamie Oliver',
-        rating: 4,
-        price: 0,
-        date: 2014,
-        cover: 'img/book-cover%20(2).png'
-    },
-    {
-        ID: 4,
-        title: 'Inexpensive Family Meals',
-        author: 'Simon Holst',
-        rating: 4,
-        price: 0,
-        date: 2010,
-        cover: 'img/book-cover%20(3).png'
-    },
-    {
-        ID: 5,
-        title: 'Paleo Slow Cooking',
-        author: 'Chrissy Gower',
-        rating: 4,
-        price: 50,
-        date: 2014,
-        cover: 'img/book-cover%20(4).png'
-    },
-    {
-        ID: 6,
-        title: 'Cook Like an Italian',
-        author: 'Tobie Puttock',
-        rating: 4,
-        price: 0,
-        date: 2013,
-        cover: 'img/book-cover%20(5).png'
-    },
-    {
-        ID: 7,
-        title: 'Suneeta Vaswani',
-        author: 'Geeta Devi',
-        rating: 5,
-        price: 75,
-        date: 2007,
-        cover: 'img/book-cover%20(6).png'
-    },
-    {
-        ID: 8,
-        title: 'Jamie Does',
-        author: 'Jamie Oliver',
-        rating: 4,
-        price: 0,
-        date: 2008,
-        cover: 'img/book-cover%20(7).png'
-    },
-    {
-        ID: 9,
-        title: 'Jamie\'s italy',
-        author: 'Jamie Oliver',
-        rating: 5,
-        price: 40,
-        date: 2011,
-        cover: 'img/book-cover%20(8).png'
-    },
-    {
-        ID: 10,
-        title: 'Vegetables Cookbook',
-        author: 'Matthew Biggs',
-        rating: 3,
-        price: 120,
-        date: 2010,
-        cover: 'img/book-cover%20(9).png'
-    },
-];
+var DataModule = (function(){
+    var books = [
+        {
+            ID: 1,
+            title: 'Jewels of Nizam',
+            author: 'Geeta Devi',
+            rating: 5,
+            price: 100,
+            date: 2015,
+            cover: 'img/book-cover.png'
+        },
+        {
+            ID: 2,
+            title: 'Cakes & Bakes',
+            author: 'Sanjeev Kapoor',
+            rating: 5,
+            price: 80,
+            date: 2012,
+            cover: 'img/book-cover%20(1).png'
+        },
+        {
+            ID: 3,
+            title: 'Jamie\'s Kitchen',
+            author: 'Jamie Oliver',
+            rating: 4,
+            price: 0,
+            date: 2014,
+            cover: 'img/book-cover%20(2).png'
+        },
+        {
+            ID: 4,
+            title: 'Inexpensive Family Meals',
+            author: 'Simon Holst',
+            rating: 4,
+            price: 0,
+            date: 2010,
+            cover: 'img/book-cover%20(3).png'
+        },
+        {
+            ID: 5,
+            title: 'Paleo Slow Cooking',
+            author: 'Chrissy Gower',
+            rating: 4,
+            price: 50,
+            date: 2014,
+            cover: 'img/book-cover%20(4).png'
+        },
+        {
+            ID: 6,
+            title: 'Cook Like an Italian',
+            author: 'Tobie Puttock',
+            rating: 4,
+            price: 0,
+            date: 2013,
+            cover: 'img/book-cover%20(5).png'
+        },
+        {
+            ID: 7,
+            title: 'Suneeta Vaswani',
+            author: 'Geeta Devi',
+            rating: 5,
+            price: 75,
+            date: 2007,
+            cover: 'img/book-cover%20(6).png'
+        },
+        {
+            ID: 8,
+            title: 'Jamie Does',
+            author: 'Jamie Oliver',
+            rating: 4,
+            price: 0,
+            date: 2008,
+            cover: 'img/book-cover%20(7).png'
+        },
+        {
+            ID: 9,
+            title: 'Jamie\'s italy',
+            author: 'Jamie Oliver',
+            rating: 5,
+            price: 40,
+            date: 2011,
+            cover: 'img/book-cover%20(8).png'
+        },
+        {
+            ID: 10,
+            title: 'Vegetables Cookbook',
+            author: 'Matthew Biggs',
+            rating: 3,
+            price: 120,
+            date: 2010,
+            cover: 'img/book-cover%20(9).png'
+        }
+    ];
+    return {get: books};
+}());
