06276c206f2180555e2c2ec4a3e981ac2900b25d
dragoplut <oleksandr_p@i.ua>
2016-02-05 20:12:03 +0200

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
-var styl = require('gulp-styl');
 var refresh = require('gulp-livereload');
 var lr = require('tiny-lr');
 var server = lr();
@@ -17,13 +16,6 @@ gulp.task('scripts', function() {
         .pipe(refresh(server))
 });
 
-gulp.task('styles', function() {
-    gulp.src(['css/**/*.css'])
-        .pipe(styl({compress : true}))
-        .pipe(gulp.dest('build'))
-        .pipe(refresh(server))
-});
-
 gulp.task('lr-server', function() {
     server.listen(35729, function(err) {
         if(err) return console.log(err);
@@ -31,13 +23,9 @@ gulp.task('lr-server', function() {
 });
 
 gulp.task('default', function() {
-    gulp.run('lr-server', 'scripts', 'styles');
+    gulp.run('lr-server', 'scripts');
 
     gulp.watch('src/**', function(event) {
         gulp.run('scripts');
     });
-
-    gulp.watch('css/**', function(event) {
-        gulp.run('styles');
-    })
 });
