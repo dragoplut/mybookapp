55ca67cecba47f6aea8c8a742ff8bc55edd37b94
dragoplut <oleksandr_p@i.ua>
2016-02-05 19:59:26 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,29 +15,29 @@ gulp.task('scripts', function() {
         .pipe(concat('dest.js'))
         .pipe(gulp.dest('build'))
         .pipe(refresh(server))
-})
+});
 
 gulp.task('styles', function() {
     gulp.src(['css/**/*.css'])
         .pipe(styl({compress : true}))
         .pipe(gulp.dest('build'))
         .pipe(refresh(server))
-})
+});
 
 gulp.task('lr-server', function() {
     server.listen(35729, function(err) {
         if(err) return console.log(err);
     });
-})
+});
 
 gulp.task('default', function() {
     gulp.run('lr-server', 'scripts', 'styles');
 
     gulp.watch('src/**', function(event) {
         gulp.run('scripts');
-    })
+    });
 
     gulp.watch('css/**', function(event) {
         gulp.run('styles');
     })
-})
+});
