55b0824c0411855b44960bcdbf9de315b92a2ba5
dragoplut <oleksandr_p@i.ua>
2016-02-07 21:12:12 +0200

    gulp sass works. vendor.js - don't work

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 gulp.task('scripts', function() {
     return gulp.src(['js/**/*.js'])
         .pipe(jshint())
@@ -10,5 +11,20 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(gulp.dest('build'))
 });
-gulp.task('default', ['scripts'], function() {
+gulp.task('vendor-scripts', function() {
+    return gulp.src(['vendor/js/**/*.js'])
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('build'))
+});
+gulp.task('styles', function(){
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+gulp.task('vendor-styles', function(){
+    gulp.src('vendor/css/**/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('./css/'))
+});
+gulp.task('default', ['scripts', 'vendor-scripts', 'styles', 'vendor-styles'], function() {
 });
